a872fbaadfae768d50f2af0173ba52931a4e764f
ASUS <aryaprasharj02@gmail.com>
2023-01-17 20:08:44 +0530

    Solution 1

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,9 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
-    public DeluxePizza(Boolean isVeg) {
+    public DeluxePizza(Boolean isVeg){
         super(isVeg);
-        // your code goes here
+        addExtraToppings(isVeg);
+        addExtraCheese();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,56 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private static boolean cheese;
+    private static boolean bag;
+    private  boolean toppings;
 
+    private int total = 0;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
     }
 
     public int getPrice(){
-        return this.price;
+
+        if(isVeg) {
+            this.price = 300;
+            total += this.price;
+            return this.price;
+        }
+        else {
+            this.price = 400;
+            this.total += this.price;
+            return this.price;
+        }
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!cheese) {
+            System.out.println("Extra Cheese Added: " + 80);
+            this.total += 80;
+            cheese = true;
+        }
     }
 
-    public void addExtraToppings(){
-        // your code goes here
+    public void addExtraToppings(boolean isVeg){
+        if(!toppings){
+            total += isVeg? 70:120;
+            System.out.println("Extra Toppings Added: " + (isVeg? 70:120));
+            toppings = true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!bag) {
+            System.out.println("Paper bag Added: " + 20);
+            this.total += 20;
+            bag = true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        this.bill = Integer.toString(this.total);
         return this.bill;
     }
 }
